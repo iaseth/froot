11e2dbf67506d875ed5e937196a6cd64a2d8a5a7
Ankur Seth <iankurseth@gmail.com>
2025-12-03 21:54:52 +0530

    Added debug/z mode

--- ebu.py
+++ ebu.py
@@ -20,22 +20,30 @@ def get_args():
 	parser.add_argument("-t", "--title", help="Optional title", default=None)
 	parser.add_argument("-d", "--description", help="Optional description", default=None)
 
+	parser.add_argument("--debug", "-z", action='store_true', help="Turn on debug mode", default=False)
+
 	return parser.parse_args()
 
 
 def main():
 	args = get_args()
-	print(args)
+	def debug(*nargs, **kwargs):
+		if args.debug:
+			print(nargs, kwargs)
+
+	debug(args)
 	soup = get_soup(args.url)
 
 	container_selector = " ".join(args.container)
 	container = soup.select_one(container_selector)
 
-	items_selector = " ".join(args.items)
+	items_selector = " ".join(args.items) if args.items else "a"
 	items = container.select(items_selector)
 
 	for i, item in enumerate(items, start=1):
-		if args.link:
+		if item.name == 'a':
+			a = item
+		elif args.link:
 			selector = " ".join(args.link)
 			a = item.select_one(selector)
 			if a and a.name != "a":
